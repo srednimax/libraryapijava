956f4585408c8c36cd46bb648a6fbea21f949ff6
srednimax <sredniamx@gmail.com>
2023-03-14 11:46:21 +0100

    Jwt auth filter

--- src/main/java/com/libraryapi/libraryapi/config/ApplicationConfig.java
+++ src/main/java/com/libraryapi/libraryapi/config/ApplicationConfig.java
@@ -1,13 +1,22 @@
 package com.libraryapi.libraryapi.config;
 
+import com.libraryapi.libraryapi.user.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
+@RequiredArgsConstructor
 public class ApplicationConfig {
+
+    private final UserRepository repository;
+
     @Bean
     public ModelMapper modelMapper() {
         ModelMapper modelMapper = new ModelMapper();
@@ -16,5 +25,11 @@ public class ApplicationConfig {
     @Bean
     public PasswordEncoder encoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        return username -> repository.findUserByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
     }
 }
